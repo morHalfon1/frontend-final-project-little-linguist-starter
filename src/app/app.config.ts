07a832fc0073e04dd0d291248c9b2845b3a55796
morHalfon1 <morh2001@gmail.com>
2024-09-16 15:51:08 +0300

    ng add @angular/fire

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -4,7 +4,24 @@ import { provideRouter, withComponentInputBinding } from '@angular/router';
 import { routes } from './app.routes';
 import { provideAnimations } from '@angular/platform-browser/animations';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes, withComponentInputBinding()), provideAnimations(), provideAnimationsAsync('noop')]
+  providers: [
+    provideRouter(routes, withComponentInputBinding()),
+    provideAnimations(),
+    provideAnimationsAsync('noop'),
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: 'little-linguist-mor-tal',
+        appId: '1:510893949297:web:d645cf15c6a72b9b4d36d0',
+        storageBucket: 'little-linguist-mor-tal.appspot.com',
+        apiKey: 'AIzaSyB3L5RMxvlDTZHrAp-OO0tajvVV9zCOd1E',
+        authDomain: 'little-linguist-mor-tal.firebaseapp.com',
+        messagingSenderId: '510893949297',
+      })
+    ),
+    provideFirestore(() => getFirestore()),
+  ],
 };
