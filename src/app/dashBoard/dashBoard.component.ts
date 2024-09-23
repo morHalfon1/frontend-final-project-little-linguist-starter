367003827589a8904cf966011ba030b902791aa3
morHalfon1 <159721735+morHalfon1@users.noreply.github.com>
2024-09-23 23:16:59 +0300

    dashboard

--- src/app/dashBoard/dashBoard.component.ts
+++ src/app/dashBoard/dashBoard.component.ts
@@ -11,4 +11,18 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   styleUrl: './dashBoard.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
-export class DashBoardComponent { }
+export class DashBoardComponent {
+  playerData = {
+    points: 98,
+    gamesPlayed: 10,
+    highestScoreGame: 'Mixed Letters',
+    lowestScoreGame: 'Word Scramble',
+    categoriesLearned: 50,
+    categoriesNotLearned: 10,
+    mostPlayedCategory: 'Animals',
+    perfectGamesPercentage: 90,
+    learnedCategoriesPercentage: 83,
+    gamesThisMonth: 5,
+    daysStrike: 3
+  };
+ }
